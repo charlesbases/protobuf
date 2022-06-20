94e7ba56524f8adb032951404a6adb7b358530fd
zhiming.sun <zhiming.sun@qq.com>
2022-06-20 11:24:09 +0800

    up

--- protoc.sh
+++ protoc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-dir="google/protobuf/extends/"
+dir="google/protobuf/plugin/"
 files=`ls ${dir} | grep '.proto'`
 
 for i in ${files}; do
   prefix=`echo ${i} | cut -d "." -f1`
 
-  protoc -I=. --gogo_out=${GOPATH}/src ${dir}/${i}
+  protoc -I=. --go_out=${GOPATH}/src ${dir}/${i}
 done
